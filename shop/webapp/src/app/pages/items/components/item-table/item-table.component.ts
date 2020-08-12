d43de4dbff10a3992d430748f1f7e339c3fbe4bc
Andrew Clark <andrew@andrews-mbp.lan>
2020-08-12 20:14:03 +0200

    item table with checkbox etc

--- shop/webapp/src/app/pages/items/components/item-table/item-table.component.ts
+++ shop/webapp/src/app/pages/items/components/item-table/item-table.component.ts
@@ -1,6 +1,8 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, ViewChild } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
+import { SelectionModel } from '@angular/cdk/collections';
+import { MatPaginator } from '@angular/material/paginator';
 
-import { Customer } from '../../models';
 import { Item } from 'build/openapi/model/item';
 
 @Component({
@@ -9,11 +11,50 @@ import { Item } from 'build/openapi/model/item';
   styleUrls: ['./item-table.component.scss']
 })
 export class ItemTableComponent implements OnInit {
-  @Input() itemTableDate: Item[];
-  public displayedColumns: string[] = ['id', 'category', 'name', 'photo', 'tags', 'status'];
-  public dataSource: Item[];
+  @Input() itemTableData: Item[];
+  public displayedColumns: string[] = ['select', 'id', 'name', 'category', 'status'];
+  public dataSource: MatTableDataSource<Item>;
+  public selection = new SelectionModel<Item>(true, []);
 
-  public ngOnInit() {
-    this.dataSource = this.itemTableDate;
+  public isShowFilterInput = false;
+
+  @ViewChild(MatPaginator, {static: true}) paginator: MatPaginator;
+
+  public ngOnInit(): void {
+    this.dataSource = new MatTableDataSource<Item>(this.itemTableData);
+
+    this.dataSource.paginator = this.paginator;
+  }
+
+  /** Whether the number of selected elements matches the total number of rows. */
+  public isAllSelected(): boolean {
+    const numSelected = this.selection.selected.length;
+    const numRows = this.dataSource.data.length;
+    return numSelected === numRows;
+  }
+
+  /** Selects all rows if they are not all selected; otherwise clear selection. */
+  public masterToggle(): void {
+    this.isAllSelected() ?
+      this.selection.clear() :
+      this.dataSource.data.forEach(row => this.selection.select(row));
+  }
+
+  /** The label for the checkbox on the passed row */
+  public checkboxLabel(row?: any): string {
+    if (!row) {
+      return `${this.isAllSelected() ? 'select' : 'deselect'} all`;
+    }
+    return `${this.selection.isSelected(row) ? 'deselect' : 'select'} row ${row.position + 1}`;
+  }
+
+  public applyFilter(event: Event): void {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
+
+  public showFilterInput(): void {
+    this.isShowFilterInput = !this.isShowFilterInput;
+    this.dataSource = new MatTableDataSource<Item>(this.itemTableData);
   }
 }
